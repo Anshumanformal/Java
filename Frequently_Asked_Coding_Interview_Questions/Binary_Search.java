a1eadf3d494c7b070d590a088d43a8ef3d5c191b
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2025-01-07 14:33:15 +0000

    added binary search

--- Frequently_Asked_Coding_Interview_Questions/Binary_Search.java
+++ Frequently_Asked_Coding_Interview_Questions/Binary_Search.java
@@ -1,11 +1,26 @@
 public class Binary_Search {
     
-     solution {
-
+     public int solution(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right-left) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1; // When the target is not found
     }
 
     public static void main(String[] args) {
         Binary_Search solution = new Binary_Search();
-        solution.solution();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 5;
+        int result = solution.solution(nums, target);
+        System.out.println("Index of target " + target + ": " + result);
+        
     }
 }
